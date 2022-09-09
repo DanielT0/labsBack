30dcab7ed8f7b36e31596b4110b8de3ff88b36e0
Daniel T <danieltorresorjuela@gmail.com>
2022-09-08 23:22:28 -0500

    Changes in server.js and database

--- index.js
+++ index.js
@@ -2,32 +2,31 @@
     Rutas de ususarios /Auth
     host + /api/auth
 */
-const express = require('express');
-require('dotenv').config();
-const sequelize = require('./database/config');
-const cors = require('cors');
+const express = require("express");
+require("dotenv").config();
+const sequelize = require("./database/config");
+const cors = require("cors");
 const fs = require("fs");
 const https = require("https");
 // const xlsx = require('node-xlsx')
 
-
-const AdminC = require('./models/AdminCentral');
-const AdminL = require('./models/AdminLab');
-const Categoria = require('./models/Categoria');
-const ElementoG = require('./models/ElementoGrupal');
-const Elemento = require('./models/ElementoIndividual');
-const Laboratorio = require('./models/Laboratorio');
-const Proyecto = require('./models/Proyecto');
-const Usuario = require('./models/User');
-const Grupo = require('./models/Grupo');
-const Prestamo = require('./models/Prestamo');
-const ElementosPrestamo = require('./models/GrupoPrestamo');
-const SolicitudUsuario = require('./models/SolicitudesUsuario');
+const AdminC = require("./models/AdminCentral");
+const AdminL = require("./models/AdminLab");
+const Categoria = require("./models/Categoria");
+const ElementoG = require("./models/ElementoGrupal");
+const Elemento = require("./models/ElementoIndividual");
+const Laboratorio = require("./models/Laboratorio");
+const Proyecto = require("./models/Proyecto");
+const Usuario = require("./models/User");
+const Grupo = require("./models/Grupo");
+const Prestamo = require("./models/Prestamo");
+const ElementosPrestamo = require("./models/GrupoPrestamo");
+const SolicitudUsuario = require("./models/SolicitudesUsuario");
 
 const app = express();
 
 // Directorio público
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //CORS (restringe peticiones desde ciertas regiones)
 app.use(cors());
@@ -41,18 +40,18 @@ app.use(cors());
 
 //Lectura y parseo del body
 app.use(express.json());
-//Rutas 
+//Rutas
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/usuarios', require('./routes/users'));
-app.use('/api/laboratorios', require('./routes/labs'));
-app.use('/api/proyectos', require('./routes/projects'));
-app.use('/api/categorias', require('./routes/categorias'));
-app.use('/api/elementos', require('./routes/elements'));
-app.use('/api/prestamos', require('./routes/prestamos'));
-app.use('/api/solicitudes-registro', require('./routes/solicitudesUsuario'));
-app.use('/api/grupos-elementos', require('./routes/gruposElementos'));
-app.use('/api/importar', require('./routes/manejoDocs'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/usuarios", require("./routes/users"));
+app.use("/api/laboratorios", require("./routes/labs"));
+app.use("/api/proyectos", require("./routes/projects"));
+app.use("/api/categorias", require("./routes/categorias"));
+app.use("/api/elementos", require("./routes/elements"));
+app.use("/api/prestamos", require("./routes/prestamos"));
+app.use("/api/solicitudes-registro", require("./routes/solicitudesUsuario"));
+app.use("/api/grupos-elementos", require("./routes/gruposElementos"));
+app.use("/api/importar", require("./routes/manejoDocs"));
 //TODO: auth // crear, login, renew
 //TODO: CURD
 
@@ -60,34 +59,32 @@ app.use('/api/importar', require('./routes/manejoDocs'));
 Usuario.hasOne(AdminC);
 AdminC.belongsTo(Usuario);
 Usuario.belongsTo(Laboratorio, {
-    foreignKey: {
-      allowNull: false,
-      defaultValue: 0
-    }
-  });
+  foreignKey: {
+    allowNull: false,
+    defaultValue: 0,
+  },
+});
 
-Usuario.belongsToMany(Laboratorio, {through: AdminL});
-Laboratorio.belongsToMany(Usuario, {through: AdminL});
+Usuario.belongsToMany(Laboratorio, { through: AdminL });
+Laboratorio.belongsToMany(Usuario, { through: AdminL });
 Grupo.belongsTo(Laboratorio);
 Laboratorio.hasMany(Proyecto);
 Elemento.belongsTo(Proyecto);
 Elemento.belongsTo(Categoria);
 Elemento.belongsTo(Grupo);
-Prestamo.belongsToMany(Elemento, { through: ElementosPrestamo})
-Elemento.belongsToMany(Prestamo, {through: ElementosPrestamo});
+Prestamo.belongsToMany(Elemento, { through: ElementosPrestamo });
+Elemento.belongsToMany(Prestamo, { through: ElementosPrestamo });
 Prestamo.belongsTo(Usuario);
 SolicitudUsuario.belongsTo(Laboratorio);
 
 sequelize
-    // .sync({ force: true })
-    .sync()
-    .then(info => {
-        // Escuchar peticiones
-        // https.createServer(options, app).listen(process.env.PORT);
-        app.listen(process.env.PORT, () => {
-            console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
-        });
-        console.log('DB online');
-        // console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
-    })
-    .catch(err => console.log(err));
+  // .sync({ force: true })
+  .sync()
+  .then((info) => {
+    // Escuchar peticiones
+    // https.createServer(options, app).listen(process.env.PORT);
+    const server = new Server(app);
+    server.listen();
+    // console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
+  })
+  .catch((err) => console.log(err));
---
+++ server.js
@@ -0,0 +1,12 @@
+class Server {
+  constructor(app) {
+    this.app = app;
+    this.port = process.env.PORT || "3000";
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`App running on port ${this.port}`);
+    });
+  }
+}
